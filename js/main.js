263fbdc727ca0e6d7ae8f4d0dff1221368de405a
nikolaichub <chub.mykola@ukr.net>
2020-07-30 22:52:55 +0300

    log out and delete buttons were created in profile setting section

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@
 document.addEventListener('DOMContentLoaded', () =>{
     console.log('welcome to my console');
 
+    let profileAccount = document.getElementById('profileAccount'),
+        profileAccountIcon = document.getElementById('profileAccountIcon');
+
+    profileAccount.addEventListener('click', ()=>{
+        profileAccountIcon.classList.toggle("profile__nav-account-btn--clicked");
+    });
+
 });
